dcfa5b0e7357e6c70e88c5bb27761e0586604850
mfyasykur <mf.yasykur@gmail.com>
2022-12-04 09:27:47 +0700

    initial commit ConfigService of MyCinema-Microservices

---
+++ src/test/java/edu/binar/challenge/MyCinemaMicroservices/ConfigService/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.binar.challenge.MyCinemaMicroservices.ConfigService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
